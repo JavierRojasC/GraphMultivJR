a99936ef2a3b8af647e533b22ac8f60d75342a91
JavierRojasC <62660573+JavierRojasC@users.noreply.github.com>
2021-05-25 13:04:23 -0500

    Update ICsimul_prop.R

--- R/ICsimul_prop.R
+++ R/ICsimul_prop.R
@@ -4,7 +4,7 @@ ICsimul_prop <- function(Nsim, n, p,conflevel=0.95){
   int <- matrix(0, Nsim,3)
   color2 <- c()
   color3 <- c()
-
+  count1 <- 0
     for (i in 1:Nsim){
       Val <- rnorm(1,n*p, sqrt(n*p*(1-p)))
 
@@ -22,12 +22,14 @@ ICsimul_prop <- function(Nsim, n, p,conflevel=0.95){
 
     if (PT$conf.int[2] < p | PT$conf.int[1]  > p){
       color3[i] <- '#822828'
+      count1=count1+1
     } else {
       color3[i] <- '#245679'
     }
-
+    Real <- (count1/Nsim)*100
     }
   Int <- data.frame(int, color2, color3)
+
   hchart(pointWidth=0,type = 'columnrange',Int,name='Intervalo',
          hcaes(x=1:length(X2),high=X2, low=X1,color=color2))%>%
     hc_add_series(Int, type='scatter', hcaes(x=1:length(X2), y=X3,color=color3), name='p estimado', color='#289B9C',
@@ -40,7 +42,8 @@ ICsimul_prop <- function(Nsim, n, p,conflevel=0.95){
                width = 3,
                zIndex = 4,
                label = list(text = "",
-                            style = list( color = '#DAE0EA', fontWeight = 'bold' )))))
+                            style = list( color = '#DAE0EA', fontWeight = 'bold' )))))%>%
+    hc_title(text=paste0('El IC de ', conflevel*100, "% contiene un valor real el ",100-Real ,"% de las veces"))
 
 }
 
